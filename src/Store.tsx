4779f4db75abdb990f7bd153ac255a5c96fc6183
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-04-30 19:14:27 +0300

    Creating an action

--- src/Store.tsx
+++ src/Store.tsx
@@ -14,7 +14,7 @@ const initialState: IState = {
     episodes: [],
     favourites: []
 };
-export const Store = React.createContext<IState>(initialState);
+export const Store = React.createContext<IState | any>(initialState);
 
 const reducer = (state: IState, action: IAction): IState => {
     // pass
@@ -27,5 +27,6 @@ const reducer = (state: IState, action: IAction): IState => {
 };
 
 export const StoreProvider = (props: any): JSX.Element => {
-    return <Store.Provider value={initialState}>{props.children}</Store.Provider>;
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+    return <Store.Provider value={{ state, dispatch }}>{props.children}</Store.Provider>;
 };
