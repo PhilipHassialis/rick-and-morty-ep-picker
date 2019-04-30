b2cdde2e5d1655e03642506efb4723156a693e1a
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-04-30 16:43:26 +0300

    Creating our reducer

--- src/Store.tsx
+++ src/Store.tsx
@@ -5,14 +5,25 @@ interface IState {
     favourites: [];
 }
 
+interface IAction {
+    type: string;
+    payload: any;
+}
+
 const initialState: IState = {
     episodes: [],
     favourites: []
 };
 export const Store = React.createContext<IState>(initialState);
 
-const reducer = () => {
+const reducer = (state: IState, action: IAction): IState => {
     // pass
+    switch (action.type) {
+        case "FETCH_DATA":
+            return { ...state, episodes: action.payload };
+        default:
+            return state;
+    }
 };
 
 export const StoreProvider = (props: any): JSX.Element => {
